12219a5d8fcb263d2a393bde2b2ae78f96b25a38
Abhijeet Kumar <VE00YM318@YMI.COM>
2022-01-18 23:15:16 +0530

    Till Collections completed

--- Day 4/src/com/day4/q1/ByteStream.java
+++ Day 4/src/com/day4/q1/ByteStream.java
@@ -1,31 +1,34 @@
 package com.day4.q1;
 
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class ByteStream {
-	
-	private static byte[] readContent(File file)
-	{
-		byte[] arr = new byte[(int)file.length()];
-		try(FileInputStream input = new FileInputStream(file)){
-			//convert file into array of bytes
+
+	private static byte[] readContent(File file) {
+		byte arr[]=null;
+		try (DataInputStream input = new DataInputStream(new FileInputStream(file))) {
+			int count = input.available();
+		    arr = new byte[count];
+			// convert file into array of bytes
 			input.read(arr);
-			//Printing in byte format
-			for(byte a: arr) {
+			// Printing in byte format
+			for (byte a : arr) {
 				System.out.print(a);
 			}
 			System.out.println();
-			//Printing in char format
-			for(byte a: arr) {
-				System.out.print((char)a);
+			// Printing in char format
+			for (byte a : arr) {
+				System.out.print((char) a);
 			}
-		}catch(Exception ex){
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		return arr;
 	}
-	
+
 	public static void main(String[] args) {
 		File file = new File("hello");
 		readContent(file);
